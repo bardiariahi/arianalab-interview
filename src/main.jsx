d8af9810b180c450884a95541f8921c0223f0ace
bardia riahi <bardiariahi@gmail.com>
2023-04-11 20:02:10 +0330

    Add: view diraction

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./component/home/index";
-import Charts from "./component/charts/index";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import './assets/styles/globall.css'
-import { store } from "./store/store";
 import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./assets/styles/globall.css";
+import { store } from "./store/store";
+import Home from "./view/home/home";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/charts",
-    element: <Charts />,
-  },
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/charts",
+        element: <Charts />,
+    },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
+    </React.StrictMode>
 );
